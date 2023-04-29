a8d8c3f2daa53f208e806d600aa23611d64a9032
UyenTran218 <traty145@mymail.unisa.edu.au>
2023-04-29 22:46:36 +0930

    Advance word search

--- S1/src/WordSearch.java
+++ S1/src/WordSearch.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class WordSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Plz enter words");
-        String words = scanner.nextLine();
+        System.out.println("Plz enter string");
+        String words = scanner.nextLine().toLowerCase();
         String[] sw = words.split(" ");
-       System.out.println(checkFirstWord(sw));
+        System.out.println("Plz enter search word");
+        String word = scanner.nextLine().toLowerCase();
+        
+       System.out.println(checkFirstWord(sw, word));
        scanner.close();
        
     }
-    public static boolean checkFirstWord(String[] sw){
-        
-        if( sw[0].equalsIgnoreCase("hello") ){
-            System.out.println(sw[0]);
-            return true;
+    public static int checkFirstWord(String[] sw, String word){
+        int count = 0;
+        for(String v:sw){
+            if(v.contains(word)){
+                count += 1;
+            }
         }
-        else {
-            System.out.println(sw[0]);
-        return false;
-        }
+        return count;         
         
     }
 }
