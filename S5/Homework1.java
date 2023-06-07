944e9e7a7084b91765856a22fc8703369482020b
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2023-06-07 17:54:02 +0930

    update Homework1 to Session 105

--- S5/Homework1.java
+++ S5/Homework1.java
@@ -2,15 +2,53 @@ package S5;
 
 public class Homework1 {
     private static int[] TEST1_ARRAY = { 12, 34, 67, 90 };
-
     private static int TEST1_EXPECTED_RESULT = 113;
+    private static int[] TEST2_ARRAY = { 15, 17, 20 };
+    private static int TEST2_EXPECTED_RESULT = 20;
 
     Homework1() {
 
     }
 
-    private int findPages(int n, int m, int[] arr) {
+    boolean checkValidity(int[] pages, int m, int n, int curr_min) {
+        int studentsRequired = 1;
+        int curr_sum = 0;
+        for (int i = 0; i < n; i++) {
+            curr_sum += pages[i];
+            if (curr_sum > curr_min) {
+                studentsRequired += 1;
+            }
+        }
+        return studentsRequired <= m;
 
+    }
+
+    private int findPages(int[] arr, int n, int m) {
+        // m = 1 -> result = sum of pages
+
+        if (m > n) {
+            return -1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        int start = arr[n - 1];
+        int end = sum;
+        int result = 0;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (checkValidity(arr, m, n, mid)) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
     }
 
     private void printTestResult(int actual, int expected) {
@@ -22,14 +60,22 @@ public class Homework1 {
 
     private void runTestCase1() {
         System.out.println("running TestCase1...");
-        int res = findPages(TEST1_ARRAY.length, 2, TEST1_ARRAY);
+        int res = findPages(TEST1_ARRAY, TEST1_ARRAY.length, 2);
         printTestResult(TEST1_EXPECTED_RESULT, res);
         System.out.println("end of TestCase1.");
+    }
+
+    private void runTestCase2() {
+        System.out.println("running TestCase2...");
+        int res = findPages(TEST2_ARRAY, TEST2_ARRAY.length, 3);
+        printTestResult(TEST2_EXPECTED_RESULT, res);
+        System.out.println("end of TestCase2.");
     }
 
     public static void main(String[] args) {
         Homework1 s = new Homework1();
         s.runTestCase1();
+        s.runTestCase2();
 
     }
 }
